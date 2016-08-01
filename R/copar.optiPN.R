80782a9a672da7f327f3f174457d2516b4c11e53
BHT <bnh.tang@gmail.com>
2016-08-01 09:22:18 +0800

    S1

--- R/copar.optiPN.R
+++
@@ -1,7 +0,0 @@
-copar.optiPN <- function(PN=BS.PN, FDR=BS.FDR, thres_fdr=5){
-  id_fdr=ifelse(FDR <= thres_fdr, 1, 0)
-  id_pn=PN * id_fdr
-  idx=which(PN==max(id_pn), arr.ind=T)
-  result <- list(MaxPeak=max(id_pn), SatisFDR=FDR[idx[1,1],idx[1,2]], Index=idx)
-  return(result)
-}
